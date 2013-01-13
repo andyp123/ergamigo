abf90d7aa75293b017362e137ba4b0ed9c7d808f
Andrew Palmer <andyp.123@gmail.com>
2013-01-13 23:45:38 +0900

    + added import_workout_data function to load csv files + added workout_data.txt file with workout output for a few workouts + other misc changes

--- src/ergamigo.py
+++ src/ergamigo.py
@@ -3,6 +3,7 @@
 
 import Tkinter  # gui kit
 import pyrow    # handles connection to ergs
+import usb.core # for USBError
 import time     # for sleep
 import datetime # for getting date and time (write to logs)
 
@@ -28,10 +29,17 @@ def main():
         while workout['state'] == 0:
             time.sleep(1)
             workout = erg.getWorkout()
-        print "Workout has begun..."
-        
+        print "-- Starting workout --"
+        print "User ID: %s" % workout['userid']
+        print "Workout Type: %s" % workout['type']
+        print "Workout State: %s" % workout['state']
+        print "Interval Type: %s" % workout['inttype']
+        print "Interval Count: %s" % workout['intcount']
+        print "Machine Status: %s" % workout['status']
+        print "----------------------"
+
         workout_data = log_workout(erg, workout)
-    except USBError as e:
+    except usb.core.USBError as e:
         print "Error: Could not get data from erg."
         exit(e)
 
@@ -42,6 +50,7 @@ def main():
     if len(workout_data) > 0:
         plot_force_curves(workout_data, 0, len(workout_data))
         now = datetime.datetime.now()
+        logdir = "./logs/"
         filename = "workout_" + now.strftime("%Y%m%d_%H%M") + ".csv"
         export_workout_data(workout_data, filename)
     else:
@@ -51,22 +60,65 @@ def main():
 def import_workout_data(filename):
     """Import workout data from a csv file."""
     workout_data = []
-    # read the file
-    # first line contains columns
-    # subsequent lines contain data. The last entry, 'Forceplot' is an array that can have 0 length and max length is undefined(?)
+    try:
+        logfile = open(filename)
+    except IOError as e:
+        "Error: Could not open file \'" + filename + "\'."
+    
+    # define a stroke object to copy and populate with data
+    line = logfile.readline()[:-1] # get field names from the first line (discarding '\n')
+    field_names = line.split(',')
+
+    line = logfile.readline()[:-1]
+    while line != "":
+        # get list of values
+        values = line.split(',')
+        # create empty stroke object
+        stroke = { 'monitor': {}, 'forceplot': [] }
+        m = stroke['monitor']
+        f = stroke['forceplot']
+        # populate stroke with values
+        i = 0
+        for fn in field_names:
+            if fn != "forceplot":
+                m[fn] = values[i]
+            i += 1
+        # get forceplot if available
+        i = len(field_names) - 1
+        if field_names[i] == "forceplot":
+            for j in range(i,len(values)):
+                f.append(values[j])
+        # append stroke to workout data
+        workout_data.append(stroke)
+        line = logfile.readline()[:-1]
+
     return workout_data
 
 
 def export_workout_data(workout_data, filename):
     """Exports workout data to a .csv file."""
+    # construct a string of field headings
+    # (output_forceplot and output_fields could later be changed to function arguments)
+    output_forceplot = True
+    output_fields = ["time","distance","spm","power","pace","calhr","calories","heartrate","status"]
+    output_fields_str = ",".join([i for i in output_fields]) + (",forceplot\n" if output_forceplot else "\n")
+
+    # write the data to a file line by line    
     outfile = open(filename, 'w')
-    outfile.write("Time,Distance,SPM,Pace,Force Plot\n")
+    outfile.write(output_fields_str)
+
     for stroke in workout_data:
-        m = stroke['monitor']   # monitor data
-        f = stroke['force']     # force plot
-        m_str = str(m['time']) + "," + str(m['distance']) + "," + str(m['spm']) + "," + str(m['pace']) + ","
-        f_str = ",".join([str(i) for i in f]) 
-        outfile.write(m_str + f_str + "\n")
+        m = stroke['monitor']
+        m_str = ""
+        for field_name in output_fields:
+            m_str += str(m[field_name]) + ","
+
+        if output_forceplot:
+            f = stroke['forceplot']
+            f_str = ",".join([str(i) for i in f]) 
+            outfile.write(m_str + f_str + "\n")
+        else:
+            outfile.write(m_str[:-1] + "\n") # remove last ',' from m_str
     outfile.close()
     print "Workout log written to file \'%s\'." % filename
 
@@ -90,10 +142,10 @@ def plot_force_curves(workout_data, first, last):
     canvas = Tkinter.Canvas(top, bg = "black", width = window_width, height = window_height)
     
     # plot first stroke in red
-    plot_force_curve(workout_data[0]['force'], canvas, window_width, window_height, "red")
+    plot_force_curve(workout_data[0]['forceplot'], canvas, window_width, window_height, "red")
     # plot other strokes in default colour
     for stroke in range(first + 1, last):
-        force_data = workout_data[stroke]['force']
+        force_data = workout_data[stroke]['forceplot']
         if len(force_data) > 1:
             plot_force_curve(force_data, canvas, window_width, window_height)
     
@@ -118,7 +170,7 @@ def plot_force_curve(force_data, canvas, window_width, window_height, line_color
 
 def log_workout(erg, workout):
     """Logs a workout and returns the data as a list of strokes (monitor, force) when finished."""
-    strokes = [] # store all strokes in here
+    workout_data = [] # store all strokes in here
     
     total_time = 0
     total_distance = 0
@@ -146,16 +198,23 @@ def log_workout(erg, workout):
         total_time += monitor['time']
         total_distance += monitor['distance']
         
-        print "[%d] time: %s, distance: %s, pace: %s" % (len(strokes) + 1, monitor['time'], monitor['distance'], monitor['pace'])
+        print "[%d] time: %s, distance: %s, pace: %s" % (len(workout_data) + 1, monitor['time'], monitor['distance'], monitor['pace'])
         # print " force [ %s ]" % (",".join([str(f) for f in force]))
 
         # write stroke data to strokes
-        stroke = { 'monitor' : monitor, 'force' : force }
-        strokes.append( stroke )
+        stroke = { 'monitor' : monitor, 'forceplot' : force }
+        workout_data.append( stroke )
 
         # update workout (so the loop exits when the workout is over)    
         workout = erg.getWorkout()
     
-    return strokes
+    print "workout: %r" % workout
+    return workout_data
+
 
 main()
+
+
+def test():
+    workout_data = import_workout_data("test.csv")
+    export_workout_data(workout_data, "test_rexport.csv")
